e86d68965f34b9d4063624cb44fe69b88aa69f43
Rodrigo Leão <rodrigo.leao@radixeng.com.br>
2023-12-26 11:11:27 -0300

    Auto-generated commit [ci skip]

--- src/app/events.ts
+++
@@ -1,9 +0,0 @@
-// Occurs when a sale is inserted from cashier
-export const ADD_SALE_EVENT = "ADD_SALE_EVENT";
-
-
-export interface HubEvent {
-    eventId: string;
-    function: Function;
-    args?: any;
-};
